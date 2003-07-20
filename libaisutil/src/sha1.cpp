05ff7e4a5c25fb2cc645cf9637ae57a1176146e4
pickle <>
2003-07-20 03:49:58 +0000

    Set the order of the longs to be forward again.. Ouch the endianess was really mucked up..

--- libaisutil/src/sha1.cpp
+++ libaisutil/src/sha1.cpp
@@ -336,8 +336,8 @@ const std::string SHA1::digest_type::toStr(const unsigned char base,
 {
    std::string output;
 
-   for (unsigned char i = 5; i != 0;) {
-      output += Utils::baseXStr(u_long[--i], base).prepad(pad, '0');
+   for (unsigned char i = 0; i < 5; ++i) {
+      output += Utils::baseXStr(u_long[i], base).prepad(pad, '0');
    }
    
    return output;
