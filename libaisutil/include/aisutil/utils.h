fc16d6a88269d2d5743213267c4d3f8792747017
pickle <>
2003-07-31 21:11:17 +0000

    Started writing documentation for Utils:: -- I'm mainly testing, though.

--- libaisutil/include/aisutil/utils.h
+++ libaisutil/include/aisutil/utils.h
@@ -27,15 +27,52 @@
 
 namespace AIS {
    namespace Util {
-      //! Utility stuff - little tid bits and what not..
+      //! Miscellaneous utilities
+      /*! This is a collection of miscellaneous utilities which don't fit in
+       * elsewhere.
+       */
       namespace Utils {
 	 //! Confirm that the given string, encoded in UTF-8, is valid
+	 /*! Validate a UTF-8 string. The given string must be encoded using
+	  * UTF-8. This will validate by looking for out-of-range encodings,
+	  * redundant encodings, or invalid and/or incomplete sequences.
+	  * 
+	  * \param str The string you wish to validate
+	  * \return Will return true if the string is valid, false if invalid
+	  */
 	 extern const bool validateUTF8(const std::string& str);
 	 
 	 //! Convert the given word (as a string) to a boolean value
+	 /*! Attempt to convert the given word into a boolean value. This will
+	  * convert words such as 'true', 'false', 'on', 'off', 'yes', and 'no'
+	  * into their appropriate boolean counterparts. If the string given
+	  * is a number, a positive number will return true, while a value of
+	  * zero or less will return false.
+	  *
+	  * If the word is unknown, or is ambiguous, then the result will not
+	  * be returned, and a value of -1 will be returned.
+	  * 
+	  * \param word A single word, or number.
+	  * \return 0 for false, 1 for true, and -1 for unknown.
+	  */
 	 extern const int toBool(const std::string& word);
 	 
 	 //! Convert the given integer to another base (up to base 85)
+	 /*! This will convert the given integer into another base, and return
+	  * it as a string. Any base up to 85 can be used.
+	  *
+	  * Conversions beyond base-36 (sexatrigesimal) must be handled in a
+	  * case-sensitive manner, as latin digits and upper-case simple latin
+	  * letters will have been used up. Lower-case latin letters are used
+	  * from base-36 to base-62 (duosexagesimal). Beyond base-62, however,
+	  * symbols from the ASCII character set are used.
+	  * 
+	  * The resultant string is guaranteed to be ASCII compatible (7-bit).
+	  * 
+	  * Note that this is NOT RFC3548 compliant (Base16, Base32 or Base64).
+	  * 
+	  * \return The converted number as a String, without any padding.
+	  */
 	 extern const String baseXStr(unsigned long number,
 				      const unsigned char base,
 				      const bool networkByteOrder = false);
