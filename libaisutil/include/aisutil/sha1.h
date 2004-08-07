4295641b4d351c8dd62c6b62d6f79dffe324804e
pickle <>
2004-08-07 01:06:17 +0000

    Okay, so I poached & modified this file from KineIRCd (http://kineircd.sf.net/) - updated the copyright info in the boiler plate so it actually matches this package.. oops!

--- libaisutil/include/aisutil/sha1.h
+++ libaisutil/include/aisutil/sha1.h
@@ -1,16 +1,15 @@
 /* $Id$
  * 
- * Copyright (c) 2001,2002,2003 KineIRCd Development Team
- * (See DEV-TEAM file for details)
+ * Copyright (c) 2001,2002,2003,2004 Alien Internet Services
  *
- * This file is a part of KineIRCd.
+ * This file is a part of LibAISutil.
  * 
- * KineIRCd is free software; you can redistribute it and/or modify
+ * LibAISutil is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  * 
- * KineIRCd is distributed in the hope that it will be useful,
+ * LibAISutil is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
